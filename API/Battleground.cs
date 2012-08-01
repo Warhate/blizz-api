10d97a3b5910f1e97870b0ab30ae4a8eee867099
Taras Prokopenko <prokopenkotg@gmail.com>
2012-08-01 11:56:50 +0300

    init project

---
+++ API/Battleground.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class Battleground
+    {
+        public string Name { get; set; }
+        public int Area { get; set; }
+        public int ControllingFaction { get; set; }
+        public int Status { get; set; }
+        public long Next { get; set; }
+    }
+}
---
+++ API/RealmResources.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public static class RealmResources
+    {
+        public enum RealmStatus
+        {
+            Unknown = -1,
+            Idle = 0,
+            Populating = 1,
+            Active = 2,
+            Concluded = 3
+        }
+
+        public static string Url = "http://us.battle.net/api/wow/realm/status";
+
+        public static string GetStatus()
+        {
+            string answer = "";
+            WebRequest request = WebRequest.Create(Url);
+
+            Stream stream = request.GetResponse().GetResponseStream();
+            if (stream != null)
+            {
+                answer = new StreamReader(stream, Encoding.UTF8).ReadToEnd();
+            }
+
+            return answer;
+        }
+    }
+}
